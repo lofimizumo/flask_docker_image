546dbaa00cf05556b4382d9a229a7e63a650f10a
mizumo <mizumo1988@gmail.com>
2023-11-06 16:11:42 +1000

    charging power changed to 800

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,11 @@ WORKDIR /notebooks
 COPY . /notebooks/
 
 # ENV CONDA_ALWAYS_YES=true
-
+RUN apk update
+RUN apk --no-cache add linux-headers gcc g++
 RUN pip install numpy==1.25.1
 RUN pip install flask
+RUN pip install pytz
 RUN pip install requests
 
 # The code to run when container is started
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import requests
 
 
-url = "https://df62c75409a1648289a3f219a687490c9.clg07azjl.paperspacegradient.com/stop"
+url = "https://df62c75409a1648289a3f219a687490c9.clg07azjl.paperspacegradient.com/start"
+# url = "http://172.17.0.2:5000"
 data = {"deviceSn":"RX2505ACA10JOA160037"}
 response = requests.post(url, json=data)
 print(response.json())
