89e221af5a3336de8578d484b035bbbcdcb80773
lofimizumo <mizumo1988@gmail.com>
2023-11-16 09:11:04 +0000

    fix bug

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 schedulers = {}
 scheduler_shawsbay = None
+thread = None
 
 
 @app.route('/')
@@ -47,21 +48,32 @@ def stop_basic_scheduler():
 
 @app.route('/start_shawsbay', methods=['POST'])
 def ai_scheduler():
+    global scheduler_shawsbay, thread
+
+    if thread and thread.is_alive():
+        return jsonify(status='error', message='Scheduler already running'), 400
+
     shawsbay_phase2_devices = ['RX2505ACA10J0A180011', 'RX2505ACA10J0A170035', 'RX2505ACA10J0A170033', 'RX2505ACA10J0A160007', 'RX2505ACA10J0A180010'] 
     scheduler_shawsbay = BatteryScheduler(
-        scheduler_type='AIScheduler', battery_sn= shawsbay_phase2_devices, test_mode=False, api_version='redx')
+        scheduler_type='AIScheduler', battery_sn=shawsbay_phase2_devices, test_mode=False, api_version='redx')
+    
     thread = Thread(target=scheduler_shawsbay.start)
-    thread.daemon = True  # This will make sure the thread exits when the main program exits
+    thread.daemon = True
     thread.start()
+
     return jsonify(status='success', message='Shawsbay Scheduler started'), 200
 
 @app.route('/stop_shawsbay', methods=['POST'])
 def stop_ai_scheduler():
-    if scheduler_shawsbay:
-        scheduler_shawsbay.stop()
+    global scheduler_shawsbay, thread
+
+    if scheduler_shawsbay and thread.is_alive():
+        scheduler_shawsbay.stop()  # Ensure this method stops the thread gracefully
+        thread.join()  # Wait for the thread to finish
         return jsonify(status='success', message='Scheduler stopped'), 200
     else:
         return jsonify(status='error', message='Shawsbay Scheduler is not running'), 404
+
 
 @app.route('/cost_savings', methods=['POST'])
 def route_cost_savings():
